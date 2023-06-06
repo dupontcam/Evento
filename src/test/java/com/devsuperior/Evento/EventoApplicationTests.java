068290fdb8dd06599e213f5bce1bec524a5eab93
Carlos Dupont <dupontcam@gmail.com>
2023-06-06 15:38:15 -0300

    Initial commit

---
+++ src/test/java/com/devsuperior/Evento/EventoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.Evento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
